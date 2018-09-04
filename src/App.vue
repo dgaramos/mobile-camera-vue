d670a77ebd47dc5dc9a75602fd767f5109fc9873
Danilo Ramos <dgaramos@gmail.com>
2018-09-04 15:32:22 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <AppCameraView/>
+  </div>
+</template>
+
+<script>
+import AppCameraView from "./components/CameraView";
+
+export default {
+  name: "App",
+  components: {
+    AppCameraView
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
